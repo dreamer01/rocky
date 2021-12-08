6ad4815a9ad686dcc7257055b5a20b4550456a22
Pushpendra Singh <psnitsri01@gmail.com>
2021-12-08 22:45:44 +0530

    Adding CountDown comp with animation

---
+++ components/CountDown/index.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+import useInterval from '../../utils/useInterval';
+import Styles from './styles.module.css';
+
+type Props = {
+  duration: number;
+  onEnd?: () => void;
+};
+
+const CountDown = ({ duration, onEnd }: Props) => {
+  const [countdown, setCountdown] = useState(duration / 1000);
+
+  const tick = () => {
+    if (countdown === 1 && intervalId) {
+      onEnd && onEnd();
+      window.clearInterval(intervalId);
+    }
+    setCountdown((c) => c - 1);
+  };
+
+  const intervalId = useInterval(tick, 1000);
+  return <h1 className={Styles.countdown}>{countdown}</h1>;
+};
+
+export default CountDown;
